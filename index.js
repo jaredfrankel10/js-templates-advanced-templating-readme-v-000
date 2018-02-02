96b58ff7841de733c915f83eb133d023b8ca42a7
jared frankel <jaredfrankel10@gmail.com>
2018-02-02 14:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ Handlebars.registerHelper('comment_body', function() {
      return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
    }
  })
-  
- 
- 
- 
+
+
+
+
  function loadIssues() {
- 
+
    //  The sample file to load.
- 
+
    // var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
    // var result = template(issue);
    // document.getElementsByTagName("main")[0].innerHTML += result;
- 
+
    const template = Handlebars.compile(document.getElementById('issue-template').innerHTML);
    // issues is an array. in the template, issues is this. calling the template, and the template encountering the each key word, iterates the template over issues, writing a template for each one.
    const result = template(issues);
